a656d8d050cb06f2cc5337eb0aacc56b90c3debf
Steven Liu <crocodilepp@gmail.com>
2021-12-21 00:11:07 +0800

    no message

---
+++ main.js
@@ -0,0 +1,10 @@
+function copyTo() {
+	var code = document.getElementById("serialNumber").value;
+    var textarea = document.createElement("textarea");
+	textarea.value = code;
+    textarea.style.fontSize = '20px';
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+}
